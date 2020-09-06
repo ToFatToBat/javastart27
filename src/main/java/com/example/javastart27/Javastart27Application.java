eae8a88a07df9f1e1484fda83684e071f40891de
ToFatToBat <kpapiernia@wp.pl>
2020-09-06 21:31:59 +0200

    Initial commit

---
+++ src/main/java/com/example/javastart27/Javastart27Application.java
@@ -0,0 +1,13 @@
+package com.example.javastart27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javastart27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javastart27Application.class, args);
+    }
+
+}
