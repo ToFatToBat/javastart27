5f9ac5eed7ef8de77faebbcb9739c3f52ccb076c
ToFatToBat <kpapiernia@wp.pl>
2020-09-20 18:22:22 +0200

    Error - ThingsToDoRepository - metoda nie powstaje

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO things_to_do (to_do, description, is_it_done, dead_line_date)
-values ('architekt', 'odebrac pozwolenia na budowe', false, '2020-09-18'), ('droga', 'zamowic gruz na droge dojazdowa', false, '2020-09-17');
+values ('architekt', 'odebrac pozwolenia na budowe', false, '2020-9-18'),
+('droga', 'zamowic gruz na droge dojazdowa', false, '2020-9-17'),
+('woda', 'skontaktowac sie z ZGK', false , '2020-9-30');
